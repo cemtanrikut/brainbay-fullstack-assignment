fcc7b31f3581138779a3435a99a8ccabc6f610af
Cem Tanrikut <cemtanrikut@gmail.com>
2025-10-30 19:18:33 +0100

    refactor

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,7 +2,7 @@ export default function Layout({ children }) {
   return (
     <div className="main">
       <div className="header">
-        <div className="header-inner">Brainbay — Full-stack Assignment</div>
+        <div className="header-inner">Brainbay Full-stack Assignment</div>
       </div>
       <div className="content">{children}</div>
     </div>
